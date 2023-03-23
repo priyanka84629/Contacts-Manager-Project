21aa46f38882a20b6f5f199e504120f816f98a56
Shubham-G97 <shubham123yg@gmail.com>
2023-03-23 18:45:00 +0530

    signup page code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {BrowserRouter,Route, Routes} from 'react-router-dom'
+import Signup from "./components/Signup";
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<>
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Signup/>}></Route>
 
-export default App;
+</Routes>
+
+
+</BrowserRouter>
+ 
+</>
+  )
+}
+export default App
---
+++ src/components/Signup.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useNavigate } from "react-router";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+function Signup() {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [valid, setValid] = useState(false);
+  const [message, setMessage] = useState({ status: "", message: "" });
+
+  function submithandler() {}
+  return (
+    <>
+      <center>
+        <main className="main-cont">
+          <section className="form-container col-lg-10">
+            <div className="row">
+              <h1>Contact Manager</h1>
+              <p>Sign Up</p>
+              <div className="form">
+                <form onSubmit={submithandler}>
+                  <div className="emails">
+                    <input
+                      className="email"
+                      type="email"
+                      name="email"
+                      placeholder="Enter your Email"
+                      required
+                      onChange={(e) =>
+                        setData({ ...data, email: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="password">
+                    <input
+                      className="pass"
+                      type="password"
+                      name="password"
+                      placeholder="Enter a Password"
+                      required
+                      onChange={(e) =>
+                        setData({ ...data, password: e.target.value })
+                      }
+                    />
+                  </div>
+                  <div className="confirm-password">
+                    <input
+                      className="cnf-pass"
+                      type="password"
+                      name="confirmPassword"
+                      placeholder="Confirm your Password"
+                      required
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
+                  <button className='signin'>Sign Up</button>
+ <Link to={'/'}><button className='signin'>Sign In</button></Link>
+                </form>
+              </div>
+            </div>
+          </section>
+        </main>
+      </center>
+    </>
+  );
+}
+export default Signup;
